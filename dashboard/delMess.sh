da0f4c376a12f077b8bb4b94dc4c81019aceaa20
Andrey <mail@ansmirnov.ru>
2014-06-03 12:50:33 +0400

    add config file

--- dashboard/delMess.sh
+++ dashboard/delMess.sh
@@ -7,7 +7,9 @@
 #
 ######################################################################
 
+source config.sh
+
 date_=`date +%s`
-echo "DELETE FROM problems2 WHERE ip_host=INET_ATON('$1') AND port='$2' AND type_event='$3';" | mysql -u... -p... syslogdog
+echo "DELETE FROM problems2 WHERE ip_host=INET_ATON('$1') AND port='$2' AND type_event='$3';" | mysql -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_TABLE
 log_date=`date`
 echo "[DEL] $log_date $1 $2" >> ./log/mainlog
